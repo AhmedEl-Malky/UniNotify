f836a7715ee1ea48edcad0ad10c5c90266fa50b9
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2025-08-20 23:07:41 +0300

    Setup App Theme + Typography and packages

---
+++ app/src/main/java/com/malky/collegealert/app/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.malky.collegealert.app.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF1E88E5)  //Primary
+val PrimaryForeground = Color(0xFFFFFFFF) //OnPrimary
+
+val Secondary = Color(0xFFFB8C00) //Secondary
+val SecondaryForeground = Color(0xFFFFFFFF) //OnSecondary
+
+val Background = Color(0xFFFFFFFF) //Background
+val Foreground = Color(0xFF252525) //OnBackground
+
+val Sidebar = Color(0xFFFAFAFA) //Surface
+val SidebarForeground = Color(0xFF252525) //OnSurface
+
+val Destructive = Color(0xFFB3261E) //Error
+val DestructiveForeground = Color.White //OnError
+
+val Border = Color(0xFFEAEAEA) // Outline
+val Input = Color(0xFFEAEAEA) // Scrim
+val Ring = Color(0xFFB5B5B5) // OutlineVariant
